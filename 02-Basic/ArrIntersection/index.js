2e991202ed51b97edb2be6f361bdc9ddbd892125
Nour Al Salhi <nuraym9998@gmail.com>
2024-09-18 13:27:11 +0300

    create intersection function

---
+++ 02-Basic/ArrIntersection/index.js
@@ -0,0 +1,9 @@
+const arrIntersection = (arr1,arr2) => {
+    const arr = []
+    for(let i=0; i <= arr1.length;i++) {
+     if (arr2.includes(arr1[i]) && !arr.includes(arr1[i])) {
+        arr.push(arr1[i]);
+      }
+    }
+    return arr
+  }
