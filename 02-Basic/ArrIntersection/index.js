c274c8304d6ee4c9787bc04eedde0de9eecb8224
Nour Al Salhi <nuraym9998@gmail.com>
2024-09-18 13:42:51 +0300

    add description comment to function

--- 02-Basic/ArrIntersection/index.js
+++ 02-Basic/ArrIntersection/index.js
@@ -1,9 +1,11 @@
-const arrIntersection = (arr1,arr2) => {
-    const arr = []
-    for(let i=0; i <= arr1.length;i++) {
-     if (arr2.includes(arr1[i]) && !arr.includes(arr1[i])) {
-        arr.push(arr1[i]);
-      }
+//Write a function called arrayIntersection that takes in two arrays and returns an array containing the intersection of the two input arrays
+
+const arrIntersection = (arr1, arr2) => {
+  const arr = [];
+  for (let i = 0; i <= arr1.length; i++) {
+    if (arr2.includes(arr1[i]) && !arr.includes(arr1[i])) {
+      arr.push(arr1[i]);
     }
-    return arr
   }
+  return arr;
+};
